4eb8e51489e4ae0d5a57bb3e034daf5d783d511b
ihorbond <ihorbond@gmail.com>
2020-05-28 16:09:30 -0400

    add responsiveness

--- js/index.js
+++ js/index.js
@@ -1,24 +1,17 @@
 
 $.when($.ready).then(function () {
+
+    $("#space-video").on('timeupdate', onBackgroundVideoUpdate);
+
+    $(window).resize(onWindowResize);
+    $(window).resize();
     $(window).on('scroll', function () {
         this.pageYOffset > 50
             ? $("#navbar").addClass('bg-light')
             : $("#navbar").removeClass('bg-light');
     });
 
-    handleBackgroundVideo();
-
-    $("#contact-form").on('submit', function (e) {
-        e.preventDefault();
-        const btn = $('#send-email-btn').get(0);
-        btn.disabled = true;
-        btn.innerHTML = '<i class="fa fa-cog fa-spin fa-2x fa-fw"></i>';
-        emailjs.sendForm('default_service', 'contact_form', this).then(res => {
-            handleEmailSent(btn, this, true);
-        }, err => {
-            handleEmailSent(btn, this, false);
-        });
-    })
+    $("#contact-form").on('submit', onContactFormSubmit);
 
     $("#home-nav").on('click', e => {
         e.preventDefault();
@@ -42,36 +35,70 @@ $.when($.ready).then(function () {
         e.preventDefault();
         $("#contact").get(0).scrollIntoView({ behavior: "smooth" });
         history.replaceState(undefined, undefined, "#contact")
-    })
+    });
 });
+
+function onContactFormSubmit(e) {
+    e.preventDefault();
+    const btn = $('#send-email-btn').get(0);
+    btn.disabled = true;
+    btn.innerHTML = '<i class="fa fa-cog fa-spin fa-2x fa-fw"></i>';
+    emailjs.sendForm('default_service', 'contact_form', this).then(
+        res => {
+            handleEmailSent(btn, this, true);
+        }, err => {
+            handleEmailSent(btn, this, false);
+        }
+    );
+}
+
+function onWindowResize(e) {
+    const btn = $("#get-quote-btn");
+    const windowWidth = $(window).width();
+    if (windowWidth < 500) {
+        console.log("hit");
+        btn.removeClass();
+        btn.addClass('btn btn-outline-light m-2 btn-sm');
+    }
+    else if (windowWidth > 500 && windowWidth < 1000) {
+        btn.removeClass();
+        btn.addClass('btn btn-outline-light m-2 btn-md');
+    }
+    else {
+        btn.removeClass();
+        btn.addClass('btn btn-outline-light m-2 btn-lg');
+    }
+
+    $("#space-video").css('width', $(window).width());
+}
 
 function handleEmailSent(btn, form, success) {
     if (success) form.reset();
-    btn.innerHTML = success ? '<span class="text-success"> Message sent! </span>' : '<span class="text-danger"> There was an error. Please try again </span>'
+    btn.innerHTML = success
+        ? '<span class="text-success"> Message sent! </span>'
+        : '<span class="text-danger"> There was an error. Please try again </span>'
+
     setTimeout(() => {
         btn.disabled = false;
         btn.innerHTML = 'Send <i class="fa fa-paper-plane-o" aria-hidden="true"></i>'
     }, 2500)
 }
 
-function handleBackgroundVideo() {
-    $("#space-video").on('timeupdate', function (e) {
-        if (e.timeStamp > 5000) {
-            this.pause();
+function onBackgroundVideoUpdate(e) {
+    if (e.timeStamp > 5000) {
+        this.pause();
 
-            this.classList.add("dim");
+        this.classList.add("dim");
 
-            $("#title").css("display", "flex")
-                .hide()
-                .fadeIn();
+        $("#title").css("display", "flex")
+            .hide()
+            .fadeIn();
 
-            $("#navbar").css("display", "flex")
-                .hide()
-                .slideDown();
-
-            // $('[data-spy="scroll"]').each(function () {
-            //     var $spy = $(this).scrollspy('refresh')
-            //   })
-        }
-    });
+        $("#navbar").css("display", "flex")
+            .hide()
+            .slideDown();
+        // $('[data-spy="scroll"]').each(function () {
+        //     var $spy = $(this).scrollspy('refresh')
+        //   })
+    }
 }
